28953b1dc04f61eb7f7d897cfdf2d747f79688f6
Hasan7584 <toimom48@gmail.com>
2024-11-23 15:32:10 +0600

    first commit

---
+++ style.js
@@ -0,0 +1,39 @@
+// const headind=document.getElementsByTagName("h1")
+// console.log(headind[1].innerText)
+
+// const bondu=document.getElementsByClassName('friend')
+
+// for(fr of bondu){
+//     console.log(fr)
+// }
+
+// const bondu=document.querySelectorAll('.friend')
+// console.log(bondu[0].innerText)
+
+// const lorem=document.getElementById('hasan')
+// console.log(lorem)
+
+
+// const first=document.getElementsByClassName('first')
+// first.innerText='hasan khan'
+
+// first.style.backgroundColor="blue"
+// first.style.color="red"
+// console.log(name.innerText)
+
+// const lorem=document.getElementById("lorem")
+// lorem.innerText='hasan khan'
+// lorem.style.backgroundColor="blue"
+// lorem.style.color="white"
+// lorem.style.fontSize="30px"
+// lorem.style.width="50%"
+// lorem.style.margin="50px"
+// lorem.style.padding="50px"
+// lorem.style.borderRadius="30px"
+// lorem.style.textAlign="center"
+// lorem.style.height="100px"
+// lorem.style.justifyContent="center"
+
+
+const contanier=document.getElementById("container")
+console.log(contanier.childNodes[0].nextSibling.innerText)
